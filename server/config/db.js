f786cd4708ffe78d95933c830ca8f6a913d7c6e7
Ragha Shruthi Sundar <raghashruthi.srs@gmail.com>
2023-03-05 20:26:04 -0500

    midterm committed

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+/*File name: COMM229-MIDTERM-301229066
+  Student's name: Ragha Shruthi Sundar
+  Student ID : 301229066
+  Web App name :Mid-Term*/
+  module.exports = {
+    "URI": "mongodb+srv://rsundar7:rhMrY93AgJT4Wu6T@clusterrs.twdcbsh.mongodb.net/books?retryWrites=true&w=majority"
+  };
+  
